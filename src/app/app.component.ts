a77e51b8242f50690c66c3adda1d4bee2241af18
Patrick-Njiru <patricknjiru93@gmail.com>
2023-02-15 08:07:59 +0300

    Create component to practice angular data binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  firstName = 'Patrick';
+  fullName = "Patrick Njiru Kung'u"
+  about = 'I live in Nairobi, Kenya'
+  clicked = ""
+  addText () { this.clicked = "I have been clicked" } 
+  // changeText () {
+    //   this.clicked = "I have been double clicked"
+    // }
+  removeText () { this.clicked = "" }
+  handleHover () { this.clicked = "Hello there!" }
+  // passValueToComponent(e) { // error caused by 'e'
+    //   console.log(e.target.value);
+  // }
+  answer = ""
+  // changeAnswer (e) { // error caused by 'e'
+  //   this.answer = e.target.value
+  // }
+
+  // exercise
+  value = ''
+  iAmDisabled = false
+  handleClick () {
+    this.value.toLowerCase() === "submitted" ? this.iAmDisabled = true : this.iAmDisabled = false
+    this.value = ''
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
